fb110adbf17f1446bf66cdae60d35950476a12b2
yoland.yao <yoland.yao@0b246485-46d8-5b5f-f173-779ef5fa0e76>
2011-10-30 18:48:57 +0000

    runtime-link=static

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd 3rd/boost
 ./bootstrap.sh
-./bjam --with-date_time --with-system --with-thread variant=release link=static threading=multi stage 
-./bjam ../.. -d2 variant=release link=static threading=multi 
+./bjam --with-date_time --with-system --with-thread variant=release link=static threading=multi runtime-link=static stage
+./bjam ../.. -d2 variant=release link=static threading=multi runtime-link=static
 cd ../..
