c3cabe56556a80654582d9052485365918d49324
yoland.yao <yoland.yao@0b246485-46d8-5b5f-f173-779ef5fa0e76>
2011-10-30 17:53:24 +0000

    for build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd 3rd/boost/
+cd 3rd/boost
 ./bootstrap.sh
 ./bjam --with-date_time --with-system --with-thread variant=release link=static threading=multi stage 
 ./bjam ../.. -d2 variant=release link=static threading=multi 
