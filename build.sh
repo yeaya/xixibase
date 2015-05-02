cd99063821a696b91323e5d859126d829189e236
Yao Yuan <yeaya@163.com>
2015-05-02 23:24:26 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+xixibaseDIR=$(pwd)
+cmake -DCMAKE_BUILD_TYPE=Release .
+#cmake .
+make
+mkdir dist
+mkdir dist/xixibase
+mkdir dist/xixibase/bin
+cp bin/xixibase dist/xixibase/bin/.
+mkdir dist/xixibase/conf
+cp conf/web.xml dist/xixibase/conf/.
+cp conf/server.xml dist/xixibase/conf/.
+cp conf/privkey.pem dist/xixibase/conf/.
+cp conf/cacert.pem dist/xixibase/conf/.
+mkdir dist/xixibase/webapps
+cp -r -f webapps/* dist/xixibase/webapps/.
+cd dist
+tar cvzf xixibase-ver-os-bin.tar.gz xixibase
+cd ..
